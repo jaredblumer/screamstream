837a2b140d7894daf3dd4b732ae7f0f4d28327d9
Jared Blumer <jaredblumer@protonmail.com>
2025-08-10 20:21:16 -0400

    Remove bottom margins

--- client/src/pages/search.tsx
+++ client/src/pages/search.tsx
@@ -10,25 +10,23 @@ export default function Search() {
     <div className="min-h-screen horror-bg">
       <Header autoFocusSearch />
 
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-        <div className="mb-8 animate-slide-up">
-          <h1 className="text-4xl font-bold text-white mb-4">
-            {searchQuery.trim().length === 0
-              ? 'Search for Streaming Horror Titles'
-              : `Search Results for "${searchQuery}"`}
-          </h1>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 animate-slide-up">
+        <h1 className="text-4xl font-bold text-white mb-4">
+          {searchQuery.trim().length === 0
+            ? 'Search for Streaming Horror Titles'
+            : `Search Results for "${searchQuery}"`}
+        </h1>
 
-          <MovieGrid
-            searchQuery={searchQuery}
-            selectedPlatform="all"
-            selectedYear="all"
-            selectedCriticsRating="all"
-            selectedUsersRating="all"
-            selectedType="all"
-            selectedSubgenre="all"
-            sortBy="rating"
-          />
-        </div>
+        <MovieGrid
+          searchQuery={searchQuery}
+          selectedPlatform="all"
+          selectedYear="all"
+          selectedCriticsRating="all"
+          selectedUsersRating="all"
+          selectedType="all"
+          selectedSubgenre="all"
+          sortBy="rating"
+        />
       </div>
 
       <Footer />
